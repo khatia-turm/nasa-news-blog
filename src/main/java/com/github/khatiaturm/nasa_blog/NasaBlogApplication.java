0a8fafe962d51433db6ed45dc44a7a1c2a8d443b
khatia-turm <turmanidzekhatia66@gmail.com>
2026-04-16 18:51:49 +0400

    initial commit

---
+++ src/main/java/com/github/khatiaturm/nasa_blog/NasaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.github.khatiaturm.nasa_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/khatiaturm/nasa_blog/NasaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.khatiaturm.nasa_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
