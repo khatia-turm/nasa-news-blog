076909c4a6066550bff6740439d457ec64a32662
khatia-turm <turmanidzekhatia66@gmail.com>
2026-04-17 17:05:18 +0400

    chore: configure logging levels and file outout

--- src/main/java/com/github/khatiaturm/nasa_blog/NasaBlogApplication.java
+++ src/main/java/com/github/khatiaturm/nasa_blog/NasaBlogApplication.java
@@ -1,13 +1,18 @@
 package com.github.khatiaturm.nasa_blog;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NasaBlogApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(NasaBlogApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(NasaBlogApplication.class, args);
+        log.info("NASA Blog application started");
     }
 
 }
